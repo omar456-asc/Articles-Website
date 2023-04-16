942db92048c144221add0ffde1ef6725d1344f6c
Omar Walid <omo_1712@yahoo.com>
2023-04-16 17:53:58 +0200

    add router

--- index.php
+++ index.php
@@ -1,9 +1,22 @@
 <?php
 require_once("vendor/autoload.php");
+session_start();
 $db = new MySQLHandler("users");
 
 $host  = $_SERVER['HTTP_HOST'];
 $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 $extra = 'views/home.php';
 header("Location: http://$host$uri/$extra");
-// header("location:views/home.php");
+
+// This is the router class
+
+
+// $router = new \config\Router();
+// $routes = require('routes.php');
+
+// $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+// $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+
+// $router->route($uri, $method);
+
+// If the team understand uncomment and start working on the router class !!!
