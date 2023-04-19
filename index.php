c6666cc9ffc8f1415e3811b9830d05929654ba3d
Mahmoud Mohamed <mahmoudmahmed1499@gmail.com>
2023-04-20 01:18:18 +0200

    fiz index

--- index.php
+++ index.php
@@ -1,21 +1,31 @@
 <?php
 require_once("vendor/autoload.php");
-<<<<<<< Updated upstream
 $db = new MySQLHandler("users");
 $host  = $_SERVER['HTTP_HOST'];
 $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 $extra = 'views/home.php';
 header("Location: http://$host$uri/$extra");
 
-// This is the router class
+
+session_start();
 
 
-// $router = new \config\Router();
-// $routes = require('routes.php');
 
-// $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-// $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+if (!isset($_SESSION['loggedin'])) {
+    if (isset($_COOKIE['loggedin'])) {
 
-// $router->route($uri, $method);
+        $data = $_COOKIE;
 
-// If the team understand uncomment and start working on the router class !!!
+        $_SESSION['loggedin'] = 'true';
+
+        foreach ($data as $k => $v) {
+            $_SESSION[$k] = $v;
+        }
+        var_dump($data);
+        header("Location: views/home.php");
+    } else {
+        header("Location: views/login.php");
+    }
+} else {
+    header("Location: views/home.php");
+}
