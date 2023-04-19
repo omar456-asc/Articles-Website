3017748defef186d7086afc1a19d291000663112
Omar Walid <omo_1712@yahoo.com>
2023-04-19 04:17:07 +0200

    fix router class

--- index.php
+++ index.php
@@ -6,15 +6,4 @@ $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
 $extra = 'views/home.php';
 header("Location: http://$host$uri/$extra");
 
-// This is the router class
-
-
-// $router = new \config\Router();
-// $routes = require('routes.php');
-
-// $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-// $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
-
-// $router->route($uri, $method);
-
-// If the team understand uncomment and start working on the router class !!!
+// Path: index.php
