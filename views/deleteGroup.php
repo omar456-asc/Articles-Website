e854268c93473240c72af314624726b47bb260be
Yousef Adel <yousefjooo2018@gmail.com>
2023-04-20 02:54:03 +0200

    add authAdminEditorMiddleware

--- views/deleteGroup.php
+++ views/deleteGroup.php
@@ -4,7 +4,7 @@ require_once("../vendor/autoload.php");
 
 require_once('components/header.php');
 
-require_once('components/authmiddleware.php');
+require_once('components/authMiddleware.php');
 
 require_once('components/sidenav.php');
 require_once('components/navbar.php');
--- views/users.php
+++ views/users.php
@@ -4,7 +4,7 @@ $db = new MySQLHandler("users");
 
 require_once('components/header.php');
 
-require_once('components/authmiddleware.php');
+require_once('components/authMiddleware.php');
 
 
 require_once('components/sidenav.php');
